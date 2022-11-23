439eb795b4062a58ab58ba57019625f6b5756606
UMASRI-07 <rumasri17@gmail.com>
2022-11-23 11:32:00 +0530

    updated

---
+++ Assignment/day01/src/prog1.c
@@ -0,0 +1,17 @@
+#include <common.h>
+
+int main()
+{
+	float PI = 3.142;
+	int it;
+	char name[MAX] = "Bhima";
+
+	it = 101;
+
+	printf("\nPI = %f\tIT=%d",PI,it);
+
+	printf("\nName: %s", name);
+	display();
+
+	return 0;
+}
